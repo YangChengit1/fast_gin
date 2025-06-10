f24f6b5900dff0c7784936160f063a94b3ed66a4
YangChengit1 <2536526382@qq.com>
2025-06-10 11:32:10 +0800

    日志25-6-10

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"fast_gin/core"
 	"fast_gin/flags"
 	"fast_gin/global"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	core.InitLogger()
 	flags.Parse()
 	global.Config = core.ReadConfig()
-	global.Config.Db.Port = 3307
+	//global.Config.Db.Port = 3307 // 这里就可以进行配置的修改
+	//core.DumpConfig() // 将修改的配置项进行赋值
+	logrus.Errorf("你好")
 }
